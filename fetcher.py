a0d370c5cd50364bbd9e056cd47d85f426284f7f
Arjun <arjunsohur@gmail.com>
2024-06-12 11:09:32 -0500

    some db functions

--- fetcher.py
+++ fetcher.py
@@ -4,16 +4,35 @@ from convokit import Corpus
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
-# OP Posts                                                                     #
+# Getting ids from command function                                            #
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
-def get_op_posts(path_to_db):
+def get_ids(command: str, path_to_db: str) -> list:
     con = sqlite3.connect(path_to_db)
     cur = con.cursor()
 
-    posts_cur = cur.execute("SELECT id FROM CMV WHERE id = root_id")
-    posts_ids = cur.fetchall()
+    posts_cur = cur.execute(command)
+    posts_ids = posts_cur.fetchall()
 
     return posts_ids
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+# get ids commands                                                             #
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+def get_op_posts(path_to_db: str) -> list:
+    return get_ids("SELECT id FROM CMV WHERE id = root_id", path_to_db)
+
+def get_success_posts(path_to_db) -> list:
+    return get_ids("SELECT id, root_id FROM CMV WHERE success = 1", path_to_db)
+
+def get_success_posts_reply_to(path_to_db) -> list:
+    return get_ids("SELECT reply_to, root_id FROM CMV WHERE success = 1", path_to_db)
+
+def get_unsuccess_posts(path_to_db) -> list:
+    return get_ids("SELECT id, root_id FROM CMV WHERE success = 0", path_to_db)
 
