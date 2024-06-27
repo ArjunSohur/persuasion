1b8dfac6995ee7f1e7bda1d5cb538911f9a5c985
Arjun Govind Sohur <arjunsohur@g009.ds.uchicago.edu>
2024-06-26 23:06:48 -0500

    change tokizer download

--- hypothesis_generation/embed.py
+++ hypothesis_generation/embed.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import os
 
-def load_custom_sentence_transformer(model_name_or_path: str = "Alibaba-NLP_gte-large-en-v1.5") -> SentenceTransformer:
+def load_custom_sentence_transformer(model_name_or_path: str = "Alibaba-NLP-gte-large-en-v1.5") -> SentenceTransformer:
     """
     Loads a SentenceTransformer model (pre-trained or custom).
 
@@ -13,22 +13,10 @@ def load_custom_sentence_transformer(model_name_or_path: str = "Alibaba-NLP_gte-
     Returns:
         Loaded SentenceTransformer model (SentenceTransformer).
     """
-    # Construct the path to the torch cache directory in the user's home directory
-    cache_folder = os.path.join(os.path.expanduser("~"), ".cache", "torch", "sentence_transformers")
-    model_path = os.path.join(cache_folder, model_name_or_path)
+    # I have device as cuda because I am running this on a gou - obviously, change this to cpu if you have a cpu
+    model = SentenceTransformer("Alibaba-NLP/gte-large-en-v1.5", cache_folder=cache_folder, trust_remote_code=True, device="cuda")
+    model.save(model_path)
 
-    if not os.path.exists(model_path):
-        print(f"Model '{model_name_or_path}' not found at '{model_path}'. Downloading...\n")
-        
-        os.makedirs(cache_folder, exist_ok=True)
+    print("Downloading Complete, processing links ...\n")
 
-        # I have device as cpu because I am running this on a mac - obviously, change this to gpu if you have a gpu
-        model = SentenceTransformer(model_name_or_path, cache_folder=cache_folder, trust_remote_code=True, device="cuda")
-        model.save(model_path)
-
-        print("Downloading Complete, processing links ...\n")
-    else:
-        print(f"Model '{model_name_or_path}' found at '{model_path}'. Loading...")
-        model = SentenceTransformer(model_path, cache_folder=cache_folder, trust_remote_code=True, device="cuda")
-        print("Loading Complete, processing links ...\n")
     return model
